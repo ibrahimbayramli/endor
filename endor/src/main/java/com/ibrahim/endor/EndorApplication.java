3db7bf6960f69ab1efb5fcc7212645158cf43ef5
IBRAHIM BAYRAMLI <ibrahimbayramli1902@gmail.com>
2023-11-08 00:45:28 +0300

    project created

---
+++ endor/src/main/java/com/ibrahim/endor/EndorApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.endor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndorApplication.class, args);
+	}
+
+}
---
+++ endor/src/test/java/com/ibrahim/endor/EndorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.endor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
